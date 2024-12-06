d1a33c756573e8e7e2ab401e13df3c257f987fef
bckben <131319923+bckben@users.noreply.github.com>
2024-12-05 20:50:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
